a5a136892e2ec153405c5abbb9689926e6dfd530
Nicolas <nicolas.foussard@outlook.fr>
2022-02-13 11:16:34 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import discord
+import os
+from dotenv import load_dotenv
+import requests
+import json
+import time
+import random
+from keep_alive import keep_alive
+
+keep_alive()
+
+client = discord.Client()
+
+def blague():
+    tmp = requests.get("https://blague.xyz/api/joke/random")
+    json_data = json.loads(tmp.text)
+    q = json_data['joke']['question']
+    r = json_data['joke']['answer']
+    return [q,r]
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("!test"):
+        await message.channel.send("tg")
+
+    if message.content.startswith("!launch lost Ark"):
+        await message.channel.send("Suite a plusieurs problèmes techniques, nous reportons le lancement de Lost Ark au 18 Mars 2024")
+    ''''
+    if "pseudo" in message.content:
+        await message.channel.send(f"{message.author.mention} tg")
+    '''
+
+    '''
+    if "cringe" in message.content:
+        await message.channel.send(file=discord.File('img/cringe.gif'))
+    '''
+
+    if "cringe" in message.content:
+        if random.randint(0,1) == 0:
+            await message.channel.send("a-t-on parlé de ppo ?")
+        else:
+            await message.channel.send("a-t-on parlé de shuttoo ?")
+
+    if message.content.startswith("!blague"):
+        tmp = blague()
+        await message.channel.send(tmp[0])
+        time.sleep(2)
+        await message.channel.send(tmp[1])
+
+load_dotenv('.env')
+client.run(os.getenv("TOKEN"))
